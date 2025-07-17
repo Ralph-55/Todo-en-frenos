5ea1f449679c35fc44a261dcf97aca131eb9b43f
Ralph-55 <rserra.0109@unimar.edu.ve>
2025-07-17 15:01:51 -0400

    crear rutas para poder ver todas las vistas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 function App() {
   return (
     <div class="flex flex-row justify-between items-center ">
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import  ReactDOM  from 'react-dom/client'
 import Login from './login.jsx'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  }
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
