8be1c1ccef9f80ac11a94d0841c08c45280a4fa2
Ralph-55 <rserra.0109@unimar.edu.ve>
2025-07-31 20:54:23 -0400

    prueba de sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+
 function App() {
   return (
-    <div class="flex flex-row justify-between items-center ">
+    <div>
+      <div class="flex flex-row justify-between items-center ">
       <div>hola</div>
       <div>menu</div>
       <div>usuario</div>
+      </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
